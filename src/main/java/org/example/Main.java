54009b181e76a8de0fd2a11843f10dfd9b09a608
Solvain <maluginm72@gmail.com>
2024-10-20 11:59:17 +0300

    abc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         String s1 = "dfgdsfgswwqosx";
@@ -13,8 +15,10 @@ public class Main {
         Box<Cat> catBox = new Box<>(cat);
         catBox.feed("Meat");
         System.out.println(horseBox);
-       char [] abc = "abc".toCharArray();
+        char[] abc = "abc".toCharArray();
+        int[] ab = new int[]{0, 1, 2, 5, 7};
+        System.out.println(Arrays.toString(ab));
         System.out.println(abc.length);
-        System.out.println(abc [2]==99);
+        System.out.println(abc[2] == 99);
     }
 }
