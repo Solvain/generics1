a59f87f5dcac8ebcea1dfde3337dbc861b3499d8
Solvain <maluginm72@gmail.com>
2024-10-28 15:12:14 +0200

    abc (#3)
    sort

--- src/main/java/org/example/arrays/ArrayParser.java
+++ src/main/java/org/example/arrays/ArrayParser.java
@@ -1,7 +1,37 @@
 package org.example.arrays;
 
+import java.util.Arrays;
+
 public class ArrayParser {
-    public static void sortArrayOfInts(int [] input) {
-        System.out.println(" let's sort this");
+    public static void sortArrayOfInts(int[] input) {
+        System.out.println("Let's sort this array:");
+
+        System.out.println("Before sorting" + Arrays.toString(input));
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i] > input[j]) {
+                    int temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
+
+                }
+            }
+        }
+        System.out.println("After sorting" + Arrays.toString(input));
+    }
+        public static void main(String[]args ) {
+            int[] array = new int[]{1, 7, 9, 2, 4, 9};
+            sortArrayOfInts(array);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
