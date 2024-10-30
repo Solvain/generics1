0508f60557175f4f59e3f5da7b790fe81ee3c42b
Solvain <maluginm72@gmail.com>
2024-10-30 16:50:57 +0200

    abc

--- src/main/java/org/example/arrays/ArrayParser.java
+++ src/main/java/org/example/arrays/ArrayParser.java
@@ -1,12 +1,44 @@
 package org.example.arrays;
 
+import java.util.Arrays;
+
 public class ArrayParser {
-    public static void sortArrayOfInts(int [] input) {
-        System.out.println(" let's sort this");
+    public static void sortArrayOfInts(int[] input) {
+        System.out.println("Let's sort this array:");
+
+        System.out.println("Before sorting" + Arrays.toString(input));
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i] > input[j]) {
+                    int temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
+
+                }
+            }
+        }
+        System.out.println("After sorting" + Arrays.toString(input));
     }
 
-    public int countLettersAfterSpecified(String inputString, char specified) {
+    public int countLettersAfterSpecified(String input, char specifiedChar) {
         //inputString ->  abcdabcd specified -> 'c' return -> 2;
-        return 0;
+        int index = input.indexOf(specifiedChar);
+
+
+        if (index == -1) {
+            return 0;
+        }
+
+
+        return input.length() - index - 1;
     }
 }
+
+
+
+
+
+
+
+
+
