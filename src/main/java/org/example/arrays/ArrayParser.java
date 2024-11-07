e405178a8632eb0e78f587b78572b1d31159a7e0
Solvain <maluginm72@gmail.com>
2024-11-07 17:30:47 +0200

    task 4

--- src/main/java/org/example/arrays/ArrayParser.java
+++ src/main/java/org/example/arrays/ArrayParser.java
@@ -1,5 +1,8 @@
 package org.example.arrays;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class ArrayParser {
 
 
@@ -30,6 +33,33 @@ public class ArrayParser {
             }
         }
         return count;
+    }
+
+    public String capitaliseLetters(String input, String indices) {
+      /*  StringBuilder builder = new  StringBuilder(input);
+        int[] indexArray = Arrays.stream(indices.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        for (int index : indexArray) {
+            if (index >= 0 && index < builder.length()) {
+                char upperChar = Character.toUpperCase(builder.charAt ( index));
+                builder.setCharAt(index, upperChar);
+            }*/
+        StringBuffer buffer = new StringBuffer(input);
+        int[] indexArray = Arrays.stream(indices.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        for (int index : indexArray) {
+            if (index >= 0 && index < buffer.length()) {
+                buffer.setCharAt(index, Character.toUpperCase(buffer.charAt(index)));
+            }
+        }
+
+
+        //                        from input  str = abcdabcd // numbers "3,4,5,7"
+        //                                         01234567
+        //  you must return string                 abcDABcD
+        return buffer.toString();
     }
 }
 
