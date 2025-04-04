8b338398f9eaf4e5159d06341b1497d08940f5dd
Solvain <maluginm72@gmail.com>
2025-04-04 20:39:10 +0300

    Files

--- src/main/java/org/example/Files/FileExamples.java
+++ src/main/java/org/example/Files/FileExamples.java
@@ -1,38 +1,15 @@
 package org.example.Files;
-
-import java.io.*;
 import java.nio.file.*;
-import java.util.List;
+import java.io.IOException;
 
 public class FileExamples {
-    public static void main(String[] args) {
-        example1();
-        example2();
-    }
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("data/example.txt");
 
-    public static void example1() {
-        Path path = Paths.get("example1.txt");
-        String content = "Тестовий файл";
-        try {
-            Files.write(path, content.getBytes());
-            System.out.println("Файл example1.txt створений");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+        Files.createDirectories(path.getParent());
+        Files.writeString(path, "Hello for example1.txt");
 
-    public static void example2() {
-        Path path = Paths.get("data", "example2.txt");
-        try {
-            if (!Files.exists(path)) {
-                Files.createDirectories(path.getParent());
-                Files.write(path, List.of("СТрока 1", "Строка 2"));
-            }
-            List<String> lines = Files.readAllLines(path);
-            System.out.println("Вміст example2.txt:");
-            lines.forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String content = Files.readString(path);
+        System.out.println("Read for data/example1.txt: " + content);
     }
 }
